efef774ad9e9b903e93d7d993a38c9043427725d
adhamito <aalmechkor2@gmail.com>
2024-10-17 20:37:49 +0100

    Updated page component to integrate sections and NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-
+import FirstSection from "./components/FirstSection";
+import SecondeSection from "./components/SecondeSection";
 
 export default function Home() {
   return (
-    <div className="p-10 bg-white w-full h-screen">
-   
+    <div className="bg-white w-full  md:m-10">
+      <div className="flex md:flex-col flex-col justify-center items-center w-full">
+        <div className="w-full">
+          <FirstSection className="w-full" />
+        </div>
+        <div className="w-full">
+          <SecondeSection className="w-full" />
+        </div>
+      </div>
     </div>
   );
 }
